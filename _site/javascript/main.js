96df0990106a02b5c4fd284d19ff408020daebee
mkess-ivy <montier.kess@gmail.com>
2021-02-20 23:30:14 -0500

    #jyz0h2 added navigation and logo

--- _site/javascript/main.js
+++ _site/javascript/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+    
     // Wait for window load
     $(window).load(function() {
         $(".se-pre-con").fadeOut("slow");;
     });
+
+
+    var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+        forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+            this.classList.toggle("is-active");
+            $(".overlay").toggleClass("overlay-open");
+        }, false);
+        });
+    }
 });
